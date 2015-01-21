183e161948824e54c33c0884ebce05afd90c03c2
leVraiBgdu33 <louisparrouy@gmail.com>
2015-01-21 12:01:38 +0100

    Mise en forme du media player
    - Images boutons
    - Seek bar
    - Reconnaissance du mot dans le titre

--- BabeDit/app/src/main/java/com/example/parrouy/babedit/ListenActivity.java
+++ BabeDit/app/src/main/java/com/example/parrouy/babedit/ListenActivity.java
@@ -1,9 +1,9 @@
 package com.example.parrouy.babedit;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.media.Image;
 import android.media.MediaPlayer;
 import android.media.audiofx.Equalizer;
 import android.media.audiofx.Visualizer;
@@ -11,108 +11,68 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.StringTokenizer;
 
 /**
  * Created by Parrouy on 20/01/2015.
  */
 public class ListenActivity extends Activity {
     private static String mFileName = null;
-    private PlayButton   mPlayButton = null;
     private MediaPlayer mPlayer = new MediaPlayer();
     private Visualizer mVisualizer;
     private Equalizer mEqualizer;
     private VisualizerView mVisualizerView;
     private static final float VISUALIZER_HEIGHT_DIP = 50f;
     private LinearLayout ll;
+    private LinearLayout titre_layout;
     SeekBar seek_bar;
     Handler seekHandler = new Handler();
-
-    private void onPlay(boolean start) {
-        if (start) {
-            startPlaying();
-        } else {
-            stopPlaying();
-        }
-    }
+    private ImageButton start;
+    private ImageButton pause;
+    private ImageButton reprendre;
+    private Button test;
+    private TextView textLeurre;
+    private TextView titre;
+    int t=0;
 
     private void startPlaying() {
         mPlayer = new MediaPlayer();
         try {
             mPlayer.setDataSource(mFileName);
             mPlayer.prepare();
+            seek_bar.setProgress(0);
             seek_bar.setMax(mPlayer.getDuration());
             mPlayer.start();
-            setupVisualizerFxAndUI();
-            mVisualizer.setEnabled(true);
         } catch (IOException e) {
             Log.e("play", "prepare() failed");
         }
-    }
+        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
 
-    private void stopPlaying() {
-        mPlayer.release();
-        mPlayer = null;
-        mVisualizer.setEnabled(false);
-    }
-
-    Runnable run = new Runnable() {
-        @Override
-        public void run()
-        {
-            seekUpdation(seek_bar,mPlayer);
-        }
-    };
-
-    public void seekUpdation(SeekBar seek, MediaPlayer m) {
-        seek.setProgress(m.getCurrentPosition());
-        seekHandler.postDelayed(run, 1000);
-    }
-
-
-    class PlayButton extends Button {
-        boolean mStartPlaying = true;
-
-        OnClickListener clicker = new OnClickListener() {
-            public void onClick(View v) {
-                onPlay(mStartPlaying);
-                if (mStartPlaying) {
-                    setText("Stop playing");
-                } else {
-                    setText("Start playing");
-                }
-                mStartPlaying = !mStartPlaying;
+            public void onCompletion(MediaPlayer mp) {
+                start.setVisibility(View.VISIBLE);
+                pause.setVisibility(View.GONE);
+                reprendre.setVisibility(View.GONE);
+                Toast.makeText(getApplicationContext(), "Lecture finie", Toast.LENGTH_SHORT).show();
             }
-        };
-
-        public PlayButton(Context ctx) {
-            super(ctx);
-            setText("Start playing");
-            setOnClickListener(clicker);
-        }
+        });
     }
 
     private void setupVisualizerFxAndUI() {
         mVisualizerView = new VisualizerView(this);
-       /* mVisualizerView.setLayoutParams(new ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                (int) (VISUALIZER_HEIGHT_DIP * getResources().getDisplayMetrics().density)));
-        ll.addView(mVisualizerView);*/
-
-        RelativeLayout.LayoutParams param = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, (int) (VISUALIZER_HEIGHT_DIP * getResources().getDisplayMetrics().density));
-        mVisualizerView.setLayoutParams(param);
         //ll.addView(mVisualizerView);
 
-        // Create the Visualizer object and attach it to our media player.
-        //YOU NEED android.permission.RECORD_AUDIO for that in AndroidManifest.xml
         mVisualizer = new Visualizer(mPlayer.getAudioSessionId());
         mVisualizer.setCaptureSize(Visualizer.getCaptureSizeRange()[1]);
         mVisualizer.setDataCaptureListener(new Visualizer.OnDataCaptureListener() {
@@ -124,6 +84,30 @@ public class ListenActivity extends Activity {
             public void onFftDataCapture(Visualizer visualizer, byte[] bytes, int samplingRate) {
             }
         }, Visualizer.getMaxCaptureRate() / 2, true, false);
+    }
+
+    public void startPlayProgressUpdater() {
+        seek_bar.setProgress(mPlayer.getCurrentPosition());
+
+        if (mPlayer.isPlaying()) {
+            Runnable notification = new Runnable() {
+                public void run() {
+                    startPlayProgressUpdater();
+                }
+            };
+            seekHandler.postDelayed(notification,1000);
+        }else{
+            mPlayer.pause();
+            seek_bar.setProgress(0);
+        }
+    }
+
+    // This is event handler thumb moving event
+    private void seekChange(View v){
+        if(mPlayer.isPlaying()){
+            SeekBar sb = (SeekBar)v;
+            mPlayer.seekTo(sb.getProgress());
+        }
     }
 
     @Override
@@ -138,32 +122,62 @@ public class ListenActivity extends Activity {
         Log.e("file",mFileName);
 
         mPlayer = new MediaPlayer();
-        seek_bar = new SeekBar(this);
-        seekUpdation(seek_bar,mPlayer);
+        seek_bar = (SeekBar) findViewById(R.id.seek_bar);
+        seek_bar.setMax(mPlayer.getDuration());
+        seek_bar.setOnTouchListener(new View.OnTouchListener() {@Override public boolean onTouch(View v, MotionEvent event) {
+            seekChange(v);
+            return false; }
+        });
+        StringTokenizer serveur = new StringTokenizer(message, ".3gp");
+        String serveurvalue = serveur.nextToken();
+        textLeurre = (TextView) findViewById(R.id.titre_texte);
+        textLeurre.setText(textLeurre.getText().toString()+serveurvalue);
 
-        RelativeLayout ll = new RelativeLayout(this);
-        ll.setBackgroundColor(Color.rgb(52, 152, 219));
+        start = (ImageButton) findViewById(R.id.start);
+        pause = (ImageButton) findViewById(R.id.pause);
+        reprendre = (ImageButton) findViewById(R.id.reprendre);
+        test = (Button) findViewById(R.id.test);
+        test.setVisibility(View.GONE);
 
-        TextView title = new TextView(this);
-        title.setText("Écouter le son");
-        title.setTextColor(Color.rgb(189, 195, 199));
-        title.setTextSize(50);
 
-        mPlayButton = new PlayButton(this);
-        RelativeLayout.LayoutParams param = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        param.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
-        mPlayButton.setLayoutParams(param);
-        ll.addView(mPlayButton);
+        start.setVisibility(View.VISIBLE);
+        pause.setVisibility(View.GONE);
+        reprendre.setVisibility(View.GONE);
 
-        param = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        param.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
-        param.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
-        title.setLayoutParams(param);
-        ll.addView(title);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startPlaying();
+                startPlayProgressUpdater();
+                setupVisualizerFxAndUI();
+                mVisualizer.setEnabled(true);
+                start.setVisibility(View.GONE);
+                pause.setVisibility(View.VISIBLE);
+            }
+        });
 
-        seek_bar.setLayoutParams(param);
-        ll.addView(seek_bar);
-        setContentView(ll);
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayer.pause();
+                t = mPlayer.getCurrentPosition();
+                pause.setVisibility(View.GONE);
+                reprendre.setVisibility(View.VISIBLE);
+            }
+        });
+
+        reprendre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayer.seekTo(t);
+                mPlayer.start();
+                startPlayProgressUpdater();
+                pause.setVisibility(View.VISIBLE);
+                reprendre.setVisibility(View.GONE);
+            }
+        });
+
+
     }
 
     @Override
